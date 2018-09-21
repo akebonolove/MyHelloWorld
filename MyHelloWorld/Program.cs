bff141a493bbe33af0cc8ad019aa0d1b0b589404
Akebonolove <Akebono.love@qq.com>
2018-09-21 23:13:34 +0800

    CMD Output "HelloWorld"

--- MyHelloWorld/Program.cs
+++ MyHelloWorld/Program.cs
@@ -8,8 +8,6 @@ namespace MyHelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-        }
+        static void Main(string[] args) => Console.WriteLine("HelloWorld");
     }
 }
